85d04eba7ed590b261512f2554372b0ab1ade8dc
MarlieR-321 <marlie.ramirez12169@est.uca.edu.ni>
2021-05-22 08:29:37 -0600

    .

--- Sistema_Kelani/frmTiposDeClientes.Designer.vb
+++ Sistema_Kelani/frmTiposDeClientes.Designer.vb
@@ -24,24 +24,24 @@ Partial Class frmTiposDeClientes
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.txtID = New System.Windows.Forms.TextBox()
-        Me.txtTIPO = New System.Windows.Forms.TextBox()
-        Me.btnGuardar = New System.Windows.Forms.Button()
-        Me.btnEditar = New System.Windows.Forms.Button()
-        Me.btnEliminar = New System.Windows.Forms.Button()
         Me.btnNuevo = New System.Windows.Forms.Button()
-        Me.IdTClienteDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.TipoClDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.txtTIPO = New System.Windows.Forms.TextBox()
         Me.TClienteBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.KelaniDataSet = New Sistema_Kelani.KelaniDataSet()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.T_ClienteTableAdapter = New Sistema_Kelani.KelaniDataSetTableAdapters.T_ClienteTableAdapter()
+        Me.IdTClienteDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TipoClDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.GroupBox1.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TClienteBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.KelaniDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox1
@@ -55,108 +55,63 @@ Partial Class frmTiposDeClientes
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 10)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(487, 173)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox1.Size = New System.Drawing.Size(414, 141)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Gestionar tipos de usuario"
         '
-        'DataGridView1
-        '
-        Me.DataGridView1.AutoGenerateColumns = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdTClienteDataGridViewTextBoxColumn, Me.TipoClDataGridViewTextBoxColumn})
-        Me.DataGridView1.DataSource = Me.TClienteBindingSource
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 191)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(487, 349)
-        Me.DataGridView1.TabIndex = 1
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(209, 46)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(28, 20)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "iD:"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(41, 81)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(196, 20)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Nombre del tipo de cliente:"
-        '
-        'txtID
-        '
-        Me.txtID.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TClienteBindingSource, "idT_Cliente", True))
-        Me.txtID.Location = New System.Drawing.Point(244, 46)
-        Me.txtID.Name = "txtID"
-        Me.txtID.ReadOnly = True
-        Me.txtID.Size = New System.Drawing.Size(61, 26)
-        Me.txtID.TabIndex = 2
-        '
-        'txtTIPO
-        '
-        Me.txtTIPO.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TClienteBindingSource, "tipo_Cl", True))
-        Me.txtTIPO.Location = New System.Drawing.Point(244, 78)
-        Me.txtTIPO.Name = "txtTIPO"
-        Me.txtTIPO.Size = New System.Drawing.Size(218, 26)
-        Me.txtTIPO.TabIndex = 3
-        '
-        'btnGuardar
-        '
-        Me.btnGuardar.Location = New System.Drawing.Point(6, 125)
-        Me.btnGuardar.Name = "btnGuardar"
-        Me.btnGuardar.Size = New System.Drawing.Size(113, 34)
-        Me.btnGuardar.TabIndex = 4
-        Me.btnGuardar.Text = "Guardar"
-        Me.btnGuardar.UseVisualStyleBackColor = True
-        '
-        'btnEditar
-        '
-        Me.btnEditar.Location = New System.Drawing.Point(125, 125)
-        Me.btnEditar.Name = "btnEditar"
-        Me.btnEditar.Size = New System.Drawing.Size(113, 34)
-        Me.btnEditar.TabIndex = 5
-        Me.btnEditar.Text = "Editar"
-        Me.btnEditar.UseVisualStyleBackColor = True
-        '
-        'btnEliminar
-        '
-        Me.btnEliminar.Location = New System.Drawing.Point(244, 125)
-        Me.btnEliminar.Name = "btnEliminar"
-        Me.btnEliminar.Size = New System.Drawing.Size(113, 34)
-        Me.btnEliminar.TabIndex = 6
-        Me.btnEliminar.Text = "Eliminar"
-        Me.btnEliminar.UseVisualStyleBackColor = True
-        '
         'btnNuevo
         '
-        Me.btnNuevo.Location = New System.Drawing.Point(363, 125)
+        Me.btnNuevo.Location = New System.Drawing.Point(313, 102)
+        Me.btnNuevo.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnNuevo.Name = "btnNuevo"
-        Me.btnNuevo.Size = New System.Drawing.Size(113, 34)
+        Me.btnNuevo.Size = New System.Drawing.Size(85, 28)
         Me.btnNuevo.TabIndex = 7
         Me.btnNuevo.Text = "Nuevo"
         Me.btnNuevo.UseVisualStyleBackColor = True
         '
-        'IdTClienteDataGridViewTextBoxColumn
+        'btnEliminar
         '
-        Me.IdTClienteDataGridViewTextBoxColumn.DataPropertyName = "idT_Cliente"
-        Me.IdTClienteDataGridViewTextBoxColumn.HeaderText = "idT_Cliente"
-        Me.IdTClienteDataGridViewTextBoxColumn.Name = "IdTClienteDataGridViewTextBoxColumn"
-        Me.IdTClienteDataGridViewTextBoxColumn.ReadOnly = True
+        Me.btnEliminar.Location = New System.Drawing.Point(203, 102)
+        Me.btnEliminar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(85, 28)
+        Me.btnEliminar.TabIndex = 6
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
         '
-        'TipoClDataGridViewTextBoxColumn
+        'btnEditar
         '
-        Me.TipoClDataGridViewTextBoxColumn.DataPropertyName = "tipo_Cl"
-        Me.TipoClDataGridViewTextBoxColumn.HeaderText = "tipo_Cl"
-        Me.TipoClDataGridViewTextBoxColumn.Name = "TipoClDataGridViewTextBoxColumn"
+        Me.btnEditar.Location = New System.Drawing.Point(103, 102)
+        Me.btnEditar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(85, 28)
+        Me.btnEditar.TabIndex = 5
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(4, 102)
+        Me.btnGuardar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(85, 28)
+        Me.btnGuardar.TabIndex = 4
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'txtTIPO
+        '
+        Me.txtTIPO.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TClienteBindingSource, "tipo_Cl", True))
+        Me.txtTIPO.Location = New System.Drawing.Point(234, 63)
+        Me.txtTIPO.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtTIPO.Name = "txtTIPO"
+        Me.txtTIPO.Size = New System.Drawing.Size(164, 26)
+        Me.txtTIPO.TabIndex = 3
         '
         'TClienteBindingSource
         '
@@ -168,24 +123,87 @@ Partial Class frmTiposDeClientes
         Me.KelaniDataSet.DataSetName = "KelaniDataSet"
         Me.KelaniDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
+        'txtID
+        '
+        Me.txtID.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.TClienteBindingSource, "idT_Cliente", True))
+        Me.txtID.Location = New System.Drawing.Point(234, 31)
+        Me.txtID.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtID.Name = "txtID"
+        Me.txtID.ReadOnly = True
+        Me.txtID.Size = New System.Drawing.Size(47, 26)
+        Me.txtID.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 66)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(196, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre del tipo de cliente:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(199, 37)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(28, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "iD:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdTClienteDataGridViewTextBoxColumn, Me.TipoClDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.TClienteBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(9, 155)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(414, 284)
+        Me.DataGridView1.TabIndex = 1
+        '
         'T_ClienteTableAdapter
         '
         Me.T_ClienteTableAdapter.ClearBeforeFill = True
         '
+        'IdTClienteDataGridViewTextBoxColumn
+        '
+        Me.IdTClienteDataGridViewTextBoxColumn.DataPropertyName = "idT_Cliente"
+        Me.IdTClienteDataGridViewTextBoxColumn.HeaderText = "idT_Cliente"
+        Me.IdTClienteDataGridViewTextBoxColumn.Name = "IdTClienteDataGridViewTextBoxColumn"
+        Me.IdTClienteDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'TipoClDataGridViewTextBoxColumn
+        '
+        Me.TipoClDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.TipoClDataGridViewTextBoxColumn.DataPropertyName = "tipo_Cl"
+        Me.TipoClDataGridViewTextBoxColumn.HeaderText = "tipo_Cl"
+        Me.TipoClDataGridViewTextBoxColumn.Name = "TipoClDataGridViewTextBoxColumn"
+        Me.TipoClDataGridViewTextBoxColumn.ReadOnly = True
+        '
         'frmTiposDeClientes
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(504, 552)
+        Me.ClientSize = New System.Drawing.Size(436, 448)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "frmTiposDeClientes"
         Me.Text = "frmTiposDeClientes"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TClienteBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.KelaniDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
